0d43de567d32bfa5de864886f0f71785fb0a67fe
Nehemiah Mekonnen <binyamm83@gmail.com>
2024-03-29 09:25:18 +0300

    LinkedList fixed remove all occurrence

---
+++ src/Check/LinkedList.java
@@ -0,0 +1,153 @@
+package Check;
+
+public class LinkedList{
+
+    private class Node{
+        int val;
+        Node next;
+
+        Node(int val){
+            this.val = val;
+        }
+    }
+
+    Node head;
+    Node tail = head;
+
+    public int count;
+
+    public boolean isEmpty(){
+        return head != null;
+    }
+    public void addFirst(int value){
+        Node node = new Node(value);
+        if (!isEmpty()) {
+            head = tail = node;
+        }
+        else {
+            node.next = head;
+            head = node;
+        }
+        count++;
+    }
+
+    public void addLast(int value){
+        Node node = new Node(value);
+        if (!isEmpty())
+            head = tail = node;
+        else {
+            tail.next = node;
+            tail = node;
+        }
+        count++;
+    }
+
+    public void removeFirst() {
+        if (!isEmpty()) throw new IllegalStateException();
+        if (head == tail){
+            head = tail = null;
+        }
+        else {
+            Node node = head.next;
+            head.next = null;
+            head = node;
+        }
+        count--;
+    }
+
+    public void removeLast() {
+        if (!isEmpty()) throw new IllegalStateException();
+        if (head == tail){
+            head = tail = null;
+        }
+        else {
+            Node current = head;
+            while (current.next != tail) {
+                current = current.next;
+            }
+            tail = current;
+            current.next = null;
+        }
+        count--;
+    }
+
+    public int indexOf(int value) {
+        int count = 0;
+        Node current = head;
+        while (current != null) {
+            if (current.val == value){
+                return count;
+            }
+            current = current.next;
+            count++;
+        }
+        return -1;
+    }
+
+    public boolean contains(int value){
+        return indexOf(value) != -1;
+    }
+
+    public int size(){
+        return count;
+    }
+
+    public int[] toArray(){
+        Node current = head;
+        int[] arr = new int[count];
+        for (int i = 0; i < count; i++){
+            arr[i] = current.val;
+            current = current.next;
+        }
+
+        return arr;
+    }
+
+    public void reverse(){
+        if (!isEmpty()) return;
+
+        Node previous = null;
+        Node current = head;
+
+        while (current != null){
+            Node next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+        tail = head;
+        tail.next = null;
+        head = previous;
+    }
+
+    public void removeAllOccurrence(int value){
+        if (!isEmpty()) return;
+        Node current = head;
+        Node previous = null;
+
+        while (current != null) {
+            Node next = current.next;
+            if (current.val == value){
+                if (current == head) {
+                    head = next;
+                    current.next = null;
+                    current = next;
+                    count--;
+                }
+                else {
+                    previous.next = next;
+                    current.next = null;
+                    current = next;
+                    count--;
+                }
+            }
+            else {
+                previous = current;
+                current = next;
+            }
+        }
+        tail = previous;
+        if(!isEmpty()) tail = null;
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,21 @@
-import java.util.*;
+import Check.LinkedList;
+
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr1 = new int[]{7,7,7,7};
-        int[] arr2 = new int[]{2,3};
-        ListNode headA = null;
-        ListNode currentA = null;
-        ListNode headB = null;
-        ListNode currentB = null;
-        for (int i = 0; i < arr1.length; i++){
-            if (headA == null){
-                headA = new ListNode(arr1[i]);
-                currentA = headA;
-                continue;
-            }
-            currentA.next = new ListNode(arr1[i]);
-            currentA = currentA.next;
-        }
-        for (int i = 0; i < arr2.length; i++){
-            if (headB == null){
-                headB = new ListNode(arr2[i]);
-                currentB = headB;
-                continue;
-            }
-            currentB.next = new ListNode(arr2[i]);
-            currentB = currentB.next;
-        }
-        LinkedListExercise exercise = new LinkedListExercise();
-        var result = exercise.removeElements(headA, 7);
-        while (result != null){
-            System.out.println(result.val);
-            result = result.next;
+        LinkedList list = new LinkedList();
+        list.addFirst(10);
+        list.addLast(20);
+        list.addLast(30);
+        list.addLast(20);
+        list.addLast(50);
+        list.addLast(20);
+        System.out.println(list.count);
+        list.removeAllOccurrence(20);
+        list.reverse();
+        var result = list.toArray();
+        for (int j : result) {
+            System.out.println(j);
         }
     }
 }
